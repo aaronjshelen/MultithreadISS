1d15cf3d55ca0c88bd493906b69eb7c5611d3d29
buknoynoynoy <vzire08@gmail.com>
2023-03-25 23:55:01 -0400

    Threading implemented, but has some bugs.

--- NEW-MT/Client.java
+++ NEW-MT/Client.java
@@ -2,11 +2,23 @@ import java.net.*;
 import java.io.*;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-public class Client {
-    
-    public static void main(String[] args) {
+import javax.swing.plaf.synth.SynthSplitPaneUI;
 
+public class Client extends Thread {
+    private String choice;
+    private String hostname;
+    private int portName;
+
+    public Client(String host, String choice, int port) {
+        this.hostname = host;
+        this.choice = choice;
+        this.portName = port;
+    }
+
+    @Override
+    public void run() {
         // if(args.length < 2) {
         //     return;
         // }
@@ -14,34 +26,29 @@ public class Client {
         // String hostname = args[0];
         // int port = Integer.parseInt(args[1]);
 
-        Scanner scnr = new Scanner(System.in);
-
-        System.out.println("Enter hostname: ");
-        String hostname = scnr.nextLine();
-        System.out.println("Enter port: ");
-        int port = scnr.nextInt();
-
-        try(Socket socket = new Socket(hostname, port)) {
+        try(Socket socket = new Socket(this.hostname, this.portName)) {
 
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
 
-            Console console = System.console();
             String text;
 
-            do {
-                text = console.readLine("Enter a number:\n1) Date and Time\n2) Uptime\n3) Memory Use\n4) Netstat\n5) Current Users\n6) Running Processes\n");
+            //System.out.println();
+            //int numClients = Integer.parseInt(console.readLine("How many clients would you like to generate (1-25)\n"));
+
+            //writer.println(Integer.toString(numClients));
+
+            text = this.choice;
+
+            //System.out.println("in run: " + text);
 
                 
-                writer.println(text);
+            writer.println(text);
 
-                InputStream input = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                String stuff = reader.readLine();
-                System.out.println(stuff);
-
-            } while(!text.equals("bye"));
-
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String stuff = reader.readLine();
+            System.out.println(stuff);
 
         } catch (UnknownHostException e) {
             System.out.println("Server not found: " + e.getMessage());
@@ -50,5 +57,36 @@ public class Client {
         }
 
     }
+    
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scnr = new Scanner(System.in);
+        String hostname, choice;
+        int port, numClients;
+
+        System.out.println("Enter hostname:");
+        hostname = scnr.nextLine();
+        System.out.println("Enter port:");
+        port = Integer.parseInt(scnr.nextLine());
+
+        do {
+
+            System.out.println("\nEnter a number:\n1) Date and Time\n2) Uptime\n3) Memory Use\n4) Netstat\n5) Current Users\n6) Running Processes");
+            choice = scnr.nextLine();
+            System.out.println("How many clients would you like to generate? (1-25):");
+            numClients = Integer.parseInt(scnr.nextLine());
+    
+            for (int i = 0; i < numClients; i++) {
+                new Client(hostname, choice, port).start();
+                
+            }
+
+            TimeUnit.SECONDS.sleep(1);
+            
+            System.out.println("Do you want to stop? (Yes or No):");
+            choice = scnr.nextLine();
+
+        } while (!(choice.equals("Yes")));
+
+    } //end main
 
 }
